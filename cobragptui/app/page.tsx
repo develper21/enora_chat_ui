0d7800d66d30148379a05b0fb6069632425b09ba
develper21 <githubnarvin@gmail.com>
2025-11-07 14:13:07 +0530

    update a page file

--- cobragptui/app/page.tsx
+++ cobragptui/app/page.tsx
@@ -1,65 +1,340 @@
-import Image from "next/image";
+'use client';
 
-export default function Home() {
+import MessageBoxChat from '@/components/MessageBox';
+import { ChatBody, OpenAIModel } from '@/types/types';
+import {
+  Button,
+  Flex,
+  Icon,
+  Img,
+  Input,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import { useState } from 'react';
+import { MdAutoAwesome, MdBolt, MdEdit, MdPerson } from 'react-icons/md';
+import Bg from '../public/img/chat/bg-image.png';
+
+export default function Chat(props: { apiKeyApp: string }) {
+  
+  const [inputOnSubmit, setInputOnSubmit] = useState<string>('');
+  const [inputCode, setInputCode] = useState<string>('');
+  const [outputCode, setOutputCode] = useState<string>('');
+  const [model, setModel] = useState<OpenAIModel>('gpt-4o');
+  const [loading, setLoading] = useState<boolean>(false);
+  const borderColor = useColorModeValue('gray.200', 'whiteAlpha.200');
+  const inputColor = useColorModeValue('navy.700', 'white');
+  const iconColor = useColorModeValue('brand.500', 'white');
+  const bgIcon = useColorModeValue(
+    'linear-gradient(180deg, #FBFBFF 0%, #CACAFF 100%)',
+    'whiteAlpha.200',
+  );
+  const brandColor = useColorModeValue('brand.500', 'white');
+  const buttonBg = useColorModeValue('white', 'whiteAlpha.100');
+  const gray = useColorModeValue('gray.500', 'white');
+  const buttonShadow = useColorModeValue(
+    '14px 27px 45px rgba(112, 144, 176, 0.2)',
+    'none',
+  );
+  const textColor = useColorModeValue('navy.700', 'white');
+  const placeholderColor = useColorModeValue(
+    { color: 'gray.500' },
+    { color: 'whiteAlpha.600' },
+  );
+  const handleTranslate = async () => {
+    let apiKey = localStorage.getItem('apiKey');
+    setInputOnSubmit(inputCode);
+
+    const maxCodeLength = model === 'gpt-4o' ? 700 : 700;
+
+    if (!apiKey?.includes('sk-')) {
+      alert('Please enter an API key.');
+      return;
+    }
+
+    if (!inputCode) {
+      alert('Please enter your message.');
+      return;
+    }
+
+    if (inputCode.length > maxCodeLength) {
+      alert(
+        `Please enter code less than ${maxCodeLength} characters. You are currently at ${inputCode.length} characters.`,
+      );
+      return;
+    }
+    setOutputCode(' ');
+    setLoading(true);
+    const controller = new AbortController();
+    const body: ChatBody = {
+      inputCode,
+      model,
+      apiKey,
+    };
+
+    const response = await fetch('./api/chatAPI', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      signal: controller.signal,
+      body: JSON.stringify(body),
+    });
+
+    if (!response.ok) {
+      setLoading(false);
+      if (response) {
+        alert(
+          'Something went wrong went fetching from the API. Make sure to use a valid API key.',
+        );
+      }
+      return;
+    }
+
+    const data = response.body;
+
+    if (!data) {
+      setLoading(false);
+      alert('Something went wrong');
+      return;
+    }
+
+    const reader = data.getReader();
+    const decoder = new TextDecoder();
+    let done = false;
+
+    while (!done) {
+      setLoading(true);
+      const { value, done: doneReading } = await reader.read();
+      done = doneReading;
+      const chunkValue = decoder.decode(value);
+      setOutputCode((prevCode) => prevCode + chunkValue);
+    }
+
+    setLoading(false);
+  };
+
+  const handleChange = (Event: any) => {
+    setInputCode(Event.target.value);
+  };
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <Flex
+      w="100%"
+      pt={{ base: '70px', md: '0px' }}
+      direction="column"
+      position="relative"
+    >
+      <Img
+        src={Bg.src}
+        position={'absolute'}
+        w="350px"
+        left="50%"
+        top="50%"
+        transform={'translate(-50%, -50%)'}
+      />
+      <Flex
+        direction="column"
+        mx="auto"
+        w={{ base: '100%', md: '100%', xl: '100%' }}
+        minH={{ base: '75vh', '2xl': '85vh' }}
+        maxW="1000px"
+      >
+        <Flex direction={'column'} w="100%" mb={outputCode ? '20px' : 'auto'}>
+          <Flex
+            mx="auto"
+            zIndex="2"
+            w="max-content"
+            mb="20px"
+            borderRadius="60px"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/8 px-5 transition-colors hover:border-transparent hover:bg-black/4 dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            <Flex
+              cursor={'pointer'}
+              transition="0.3s"
+              justify={'center'}
+              align="center"
+              bg={model === 'gpt-4o' ? buttonBg : 'transparent'}
+              w="174px"
+              h="70px"
+              boxShadow={model === 'gpt-4o' ? buttonShadow : 'none'}
+              borderRadius="14px"
+              color={textColor}
+              fontSize="18px"
+              fontWeight={'700'}
+              onClick={() => setModel('gpt-4o')}
+            >
+              <Flex
+                borderRadius="full"
+                justify="center"
+                align="center"
+                bg={bgIcon}
+                me="10px"
+                h="39px"
+                w="39px"
+              >
+                <Icon
+                  as={MdAutoAwesome}
+                  width="20px"
+                  height="20px"
+                  color={iconColor}
+                />
+              </Flex>
+              GPT-4o
+            </Flex>
+            <Flex
+              cursor={'pointer'}
+              transition="0.3s"
+              justify={'center'}
+              align="center"
+              bg={model === 'gpt-3.5-turbo' ? buttonBg : 'transparent'}
+              w="164px"
+              h="70px"
+              boxShadow={model === 'gpt-3.5-turbo' ? buttonShadow : 'none'}
+              borderRadius="14px"
+              color={textColor}
+              fontSize="18px"
+              fontWeight={'700'}
+              onClick={() => setModel('gpt-3.5-turbo')}
+            >
+              <Flex
+                borderRadius="full"
+                justify="center"
+                align="center"
+                bg={bgIcon}
+                me="10px"
+                h="39px"
+                w="39px"
+              >
+                <Icon
+                  as={MdBolt}
+                  width="20px"
+                  height="20px"
+                  color={iconColor}
+                />
+              </Flex>
+              GPT-3.5
+            </Flex>
+          </Flex>
+        </Flex>
+        <Flex
+          direction="column"
+          w="100%"
+          mx="auto"
+          display={outputCode ? 'flex' : 'none'}
+          mb={'auto'}
+        >
+          <Flex w="100%" align={'center'} mb="10px">
+            <Flex
+              borderRadius="full"
+              justify="center"
+              align="center"
+              bg={'transparent'}
+              border="1px solid"
+              borderColor={borderColor}
+              me="20px"
+              h="40px"
+              minH="40px"
+              minW="40px"
+            >
+              <Icon
+                as={MdPerson}
+                width="20px"
+                height="20px"
+                color={brandColor}
+              />
+            </Flex>
+            <Flex
+              p="22px"
+              border="1px solid"
+              borderColor={borderColor}
+              borderRadius="14px"
+              w="100%"
+              zIndex={'2'}
+            >
+              <Text
+                color={textColor}
+                fontWeight="600"
+                fontSize={{ base: 'sm', md: 'md' }}
+                lineHeight={{ base: '24px', md: '26px' }}
+              >
+                {inputOnSubmit}
+              </Text>
+              <Icon
+                cursor="pointer"
+                as={MdEdit}
+                ms="auto"
+                width="20px"
+                height="20px"
+                color={gray}
+              />
+            </Flex>
+          </Flex>
+          <Flex w="100%">
+            <Flex
+              borderRadius="full"
+              justify="center"
+              align="center"
+              bg={'linear-gradient(15.46deg, #4A25E1 26.3%, #7B5AFF 86.4%)'}
+              me="20px"
+              h="40px"
+              minH="40px"
+              minW="40px"
+            >
+              <Icon
+                as={MdAutoAwesome}
+                width="20px"
+                height="20px"
+                color="white"
+              />
+            </Flex>
+            <MessageBoxChat output={outputCode} />
+          </Flex>
+        </Flex>
+        <Flex
+          ms={{ base: '0px', xl: '60px' }}
+          mt="20px"
+          justifySelf={'flex-end'}
+        >
+          <Input
+            minH="54px"
+            h="100%"
+            border="1px solid"
+            borderColor={borderColor}
+            borderRadius="45px"
+            p="15px 20px"
+            me="10px"
+            fontSize="sm"
+            fontWeight="500"
+            _focus={{ borderColor: 'none' }}
+            color={inputColor}
+            _placeholder={placeholderColor}
+            placeholder="Type your message here..."
+            onChange={handleChange}
+          />
+          <Button
+            variant="primary"
+            py="20px"
+            px="16px"
+            fontSize="sm"
+            borderRadius="45px"
+            ms="auto"
+            w={{ base: '160px', md: '210px' }}
+            h="54px"
+            _hover={{
+              boxShadow:
+                '0px 21px 27px -10px rgba(96, 60, 255, 0.48) !important',
+              bg: 'linear-gradient(15.46deg, #4A25E1 26.3%, #7B5AFF 86.4%) !important',
+              _disabled: {
+                bg: 'linear-gradient(15.46deg, #4A25E1 26.3%, #7B5AFF 86.4%)',
+              },
+            }}
+            onClick={handleTranslate}
+            isLoading={loading ? true : false}
           >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+            Submit
+          </Button>
+        </Flex>
+      </Flex>
+    </Flex>
   );
 }
